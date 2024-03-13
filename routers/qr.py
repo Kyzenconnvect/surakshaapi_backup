3643c8806c9a417bdc6d4432fef79c08c396aa1c
Sujoy Nath <mityblur@gmail.com>
2024-03-13 09:44:49 +0530

    Added gpay, paytm, phonepe as ok

--- routers/qr.py
+++ routers/qr.py
@@ -43,11 +43,19 @@ model = genai.GenerativeModel(model_name="gemini-1.0-pro",
 convo = model.start_chat(history=[
   {
     "role": "user",
-    "parts": ["I am giving you extracted data from a qr code. It may contains urls or payment codes and links. You perform regex and keyword based analysis and using ML techniques according to your own database to detect if this is possible fraud and categorize it in any following two category. OK, Fraud. Your will return only a json containing a key tag with proper category tag and a key message with details of your detection. Qr data: http://qr-mal.payyytm.org. Strictly follow the format and don't return the text saying json before the actual data. Format: {\"tag\": \"fraud\", \"message\": \"details\"}"]
+    "parts": ["I am giving you extracted data from a qr code. It may contains urls or payment codes and links. You perform regex and keyword based analysis and using ML techniques according to your own database to detect if this is possible fraud and categorize it in any following two category. OK, Fraud. Your will return only a json containing a key tag with proper category tag and a key message with details of your detection. Qr data: upi://pay?pa=9163619700@paytm&pn=PaytmUser&mc=0000&mode=02&purpose=00&orgid=159761&cust=1153763655. Strictly follow the format and don't return the text saying json before the actual data. Format: {\\\"tag\\\": \\\"fraud\\\", \\\"message\\\": \\\"details\\\"}. Don't flag any gpay, paytm or phonepay payment qr as fraud."]
   },
   {
     "role": "model",
-    "parts": ["{\"tag\": \"fraud\", \"message\": \"The QR code contains a malicious URL that directs to a fake payment gateway.\"}"]
+    "parts": ["{\"tag\": \"OK\", \"message\": \"No fraud detected\"}"]
+  },
+  {
+    "role": "user",
+    "parts": ["qr_data: https://payment-gatemal.way.org"]
+  },
+  {
+    "role": "model",
+    "parts": ["{\"tag\": \"fraud\", \"message\": \"Suspicious URL detected\"}"]
   },
 ])
 
